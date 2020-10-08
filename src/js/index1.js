1dd86f7066c82c74ec1c20ac658e4ed2d3b8a29b
pengshaohua <294829596@qq.com>
2020-10-08 22:32:56 +0800

    第四次

---
+++ src/js/index1.js
@@ -0,0 +1,19 @@
+// 手机二维码
+$(".icon_phone1").hover(function() {
+    $(".wx img").show()
+}, function() {
+    $(".wx img").hide()
+})
+
+// 购物车显示效果
+$(".icon_cart").hover(function() {
+    $(".cart_login").show()
+}, function() {
+    $(".cart_login").hide()
+})
+
+$(".cart_login").hover(function() {
+    $(this).show()
+}, function() {
+    $(this).hide()
+})
