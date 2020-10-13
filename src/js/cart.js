39e4764902b26d6d06d00c7de38067850e1bf43d
pengshaohua <294829596@qq.com>
2020-10-13 22:15:13 +0800

    第六次

--- src/js/cart.js
+++ src/js/cart.js
@@ -10,7 +10,6 @@ define(['jquery', 'jq_cookie'], function() {
                     rendercart(sid[i], num[i])
                 }
             }
-            console.log(2);
 
             // 2.封装函数进行渲染
             function rendercart(sid, num) { //sid:渲染的商品编号    num:渲染的商品的数量。
@@ -18,40 +17,103 @@ define(['jquery', 'jq_cookie'], function() {
                     url: 'http://localhost/8-17_HTML/Midea/php/cart.php',
                     dataType: 'json'
                 }).done(function(data) {
+
                     $.each(data, function(index, value) {
                         if (value.sid == sid) { //数据接口的sid和当前商品的sid进行比较，如果相等，直接赋值。
                             let strhtml = '';
                             strhtml += `
-                            <td class="td-product"><img src="${value.url}" width="98" height="98">
+                            <tr v-for="(item,index) in productList":key="index" class="goods">
+                                <td class="td-check"><input type="checkbox" class="check-span checked=""  check-true"></td>
+                                <td class="td-product"><img src="${value.url}" width="98" height="98">
                                 <div class="product-info">
                                     <h3>${value.title}</h3>
                                 </div>
                                 <div class="clearfix"></div>
-                            </td>
-                            <td class="td-num">
+                                </td>
+                                <td class="td-num">
                                 <div class="product-num">
                                     <a href="javascript:;" class="num-reduce num-do fl" @click="minus(index)"><span></span></a>
-                                    <input type="text" class="num-input" v-model="item.pro_num" @input="search($event,index)" @blur="check($event,index)">
+                                    <input type="text" value="${num}" class="num-input">
                                     <a href="javascript:;" class="num-add num-do fr" @click="add(index)"><span></span></a>
                                 </div>
-                            </td>
-                            <td class="td-price">
-                             
+                                 </td>
+                                <td class="td-price">
                                 <p class="red-text">￥<span class="price-text">${value.price}</span></p>
-                            </td>
-                            <td class="td-total">
+                                </td>
                             
+                                <td class="td-total">
                                 <p class="red-text">￥<span class="total-text">${(value.price*num).toFixed(2)}</span></p>
-                            </td>
+                                </td>
+                                </td>
+                                <td class="td-do"><a href="javascript:;" class="product-delect"  @click="deleteOneProduct(index)">删除</a></td>
+                            </tr>
+                        
                          `;
-                            $('.fr check-num').append(strhtml);
-                            calc(); //总算总价
-
-
+                            $('.cart-product tbody').append(strhtml);
+                            // calc(); //总算总价
                         }
                     });
+                    // 1,点击全选按钮
+                    var all = $('.check-all ');
+                    var inputs = $('.check-span').not('.check-all')
+                    all.on('click', function() {
+                        inputs.prop('checked', $(this).prop('checked'));
+                    })
+
+                    //2.下面所有的复选框选中，全选对应的选择
+                    //除了全选，选中的长度等于复选框的长度，全选勾选。
+                    inputs.on('click', function() {
+                        if ($('input:checked').not('.check-all').size() === inputs.length) {
+                            all.prop('checked', true);
+                        } else {
+                            all.prop('checked', false);
+                        }
+                    });
+
+                    //计算总的商品件数和总价。
+                    // function priceall() {
+                    //     var $sum = 0;
+                    //     var $count = 0;
+                    //     $('.goods').each(function(index, element) {
+                    //         if ($(element).find('.td-check input').prop('checked')) {
+                    //             $sum += parseInt($(element).find('.product-num').find('.num-input').val());
+                    //             $count += parseFloat($(element).find('.red-text').find('.total-text').html());
+                    //         }
+                    //     });
+
+                    //     $('.check-num').find('em').html($sum);
+                    //     $('.product-total').html('￥' + $count.toFixed(2));
+                    // }
+
+
                 })
             }
+
+            // 3.购物车其他功能
+
+            // 商品数量加减
+            $('.cart-product tbody').on('click', '.num-reduce', function() {
+                $('.num-input').val(Number($('.num-input').val()) - 1);
+                if ($('.num-input').val() == 0) {
+                    $('.num-input').val(1)
+
+                }
+
+            })
+            $('.cart-product tbody').on('click', '.num-add', function() {
+                $('.num-input').val(Number($('.num-input').val()) + 1);
+
+            })
+
+
+            // 删除单个商品
+            $('.cart-product tbody').on('click', '.product-delect', function() {
+                alert('确定要删除嘛？')
+                $(this).parent().parent().remove()
+            })
+
+
+
 
         }()
     }
--- src/js/datail_main.js
+++ src/js/datail_main.js
@@ -14,6 +14,7 @@ define(['jquery', 'jq_cookie'], function() {
                 },
                 dataType: 'json'
             }).done(function(data) {
+                console.log(1);
                 $('.spic img').attr('src', data.url);
                 $('.bf img').attr('src', data.url);
                 $('.loadtitle').html(data.title);
@@ -29,6 +30,56 @@ define(['jquery', 'jq_cookie'], function() {
                 });
                 $('.list .Chart').html(strhtml);
             });
+
+            // 数量加减
+            $('.plus').click(function() {
+                $('.num').val(Number($('.num').val()) + 1);
+            })
+            $('.mins').click(function() {
+                $('.num').val(Number($('.num').val()) - 1);
+                if ($('.num').val() == 0) {
+                    $('.num').val(1)
+                }
+            })
+
+            // 购物车下单商品存储到cookie
+
+            let arrnum = [];
+            let arrsid = [];
+
+            function getcookie() {
+                if ($.cookie('cookiesid') && $.cookie('cookienum')) { //cookie存在
+                    arrsid = $.cookie('cookiesid').split(','); //获取cookie的sid，存放到数组中。
+                    arrnum = $.cookie('cookienum').split(','); //获取cookie的数量，存放到数组中。
+                } else { //cookie不存在
+                    arrsid = [];
+                    arrnum = [];
+                }
+            }
+
+            $('.p-btn a').on('click', function() {
+                getcookie(); //如果cookie存在，取到cookie的值，并且变成了数组。
+                //如果arrsid里面存在当前商品的sid，说明商品已经存在，否则商品是第一次购买。
+                //$.inArray(value,array)确定第一个参数在数组中的位置，从0开始计数(如果没有找到则返回 -1 )。
+                //value:查找的值
+                //array:数组
+                if ($.inArray(sid, arrsid) === -1) { //不存在，将商品的sid和数量存入cookie
+                    arrsid.push(sid); //添加当前商品的sid
+                    $.cookie('cookiesid', arrsid, { expires: 10, path: '/' }); //插件完成的cookie的添加。
+                    arrnum.push($('.num').val()); //添加商品的数量
+                    $.cookie('cookienum', arrnum, { expires: 10, path: '/' }); //插件完成的cookie的添加。
+                } else { //存在,商品的数量累加
+                    //获取原来的sid对应的数量(sid和数量是对应的 ，sid的在数组的位置就是数量在数组的位置)
+                    let index = $.inArray(sid, arrsid); //sid在数组中的位置
+                    let num = parseInt(arrnum[index]); //sid对应的数量
+                    //原来的数量+新添加数量，一起存入cookie
+                    arrnum[index] = num + parseInt($('.num').val()); //原来的数量+新添加数量进行赋值
+                    $.cookie('cookienum', arrnum, { expires: 10, path: '/' }); //一起存入cookie
+                }
+
+
+            })
+
         }()
     }
 });
